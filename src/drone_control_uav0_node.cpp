1510028a8fea9a5537cf76717d74fe91a2851074
zameel <zameelnm96@gmail.com>
2020-05-21 06:53:48 +0530

    drone intergchange their positio with start and new

--- src/drone_control_uav0_node.cpp
+++ src/drone_control_uav0_node.cpp
@@ -4,6 +4,7 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <string>
 
 mavros_msgs::State current_state;
 
@@ -45,6 +46,7 @@ int main(int argc, char **argv)
   pose.pose.position.y = 0;
   pose.pose.position.z = 2;
 
+
   geometry_msgs::Twist vel;
   vel.linear.x = 0.0;
   vel.linear.y = 0.0;
@@ -54,7 +56,7 @@ int main(int argc, char **argv)
   vel.angular.z = 0.0;
 
   //send a few setpoints before starting
-  for(int i = 100; ros::ok() && i > 0; --i){
+  for(int i = 100;loop_time_start ros::ok() && i > 0; --i){
     local_pos_pub.publish(pose);
     ros::spinOnce();
     rate.sleep();
@@ -66,44 +68,74 @@ int main(int argc, char **argv)
   mavros_msgs::CommandBool arm_cmd;
   arm_cmd.request.value = true;
 
+
+  geometry_msgs::PoseStamped pose1;
+  pose.pose.position.x = 2;
+  pose.pose.position.y = 2;
+  pose.pose.position.z = 2;
+
   ros::Time last_request = ros::Time::now();
 
   ros::Time time_start = ros::Time::now();
+  ros::Time time_start1 = ros::Time::now();
+  bool isArnmed = false;
+  int  countPosition = 0;
   while(ros::ok()){
-    if( current_state.mode != "OFFBOARD" &&
-	(ros::Time::now() - last_request > ros::Duration(5.0))){
-      if( set_mode_client.call(offb_set_mode) &&
-	  offb_set_mode.response.mode_sent){
-	ROS_INFO("Offboard enabled");
+
+      ros::Time loop_time_start = ros::Time::now();
+      if( current_state.mode != "OFFBOARD" &&
+        	(ros::Time::now() - last_request > ros::Duration(5.0))){
+              if( set_mode_client.call(offb_set_mode) &&
+                	  offb_set_mode.response.mode_sent){
+                   	ROS_INFO("Offboard enabled");
+
+              }
+              last_request = ros::Time::now();
+      } else {
+              if( !current_state.armed &&
+        	  (ros::Time::now() - last_request > ros::Duration(5.0))){
+                	if( arming_client.call(arm_cmd) &&
+                	    arm_cmd.response.success){
+                	  ROS_INFO("Vehicle armed");
+                    time_start1 = ros::Time::now();
+                	}
+          	       last_request = ros::Time::now();
+              }
       }
-      last_request = ros::Time::now();
-    } else {
-      if( !current_state.armed &&
-	  (ros::Time::now() - last_request > ros::Duration(5.0))){
-	if( arming_client.call(arm_cmd) &&
-	    arm_cmd.response.success){
-	  ROS_INFO("Vehicle armed");
-	}
-	last_request = ros::Time::now();
+
+      // Update the desired pose:
+      /*pose.pose.position.x = 9*sin(2.0*M_PI*0.3*(ros::Time::now()-time_start).toSec());
+      pose.pose.position.y = 9*cos(2.0*M_PI*0.3*(ros::Time::now()-time_start).toSec());
+
+      //Update the desired velocity:
+      vel.linear.x = 4.0*M_PI*0.1*cos(2.0*M_PI*0.1*(ros::Time::now()-time_start).toSec());
+      vel.linear.y = -4.0*M_PI*0.1*sin(2.0*M_PI*0.1*(ros::Time::now()-time_start).toSec());*/
+      if(countPosition == 0){
+        local_pos_pub.publish(pose);
       }
+      if(current_state.armed && ros::Time::now() - time_start1 > ros::Duration(15.0)){
+        countPosition += 1  ;
+        if(countPosition == 2){
+          countPosition = 0;
+          ROS_INFO("countPosition 0"  );
+          time_start1 = ros::Time::now();
+        }
+      }
+
+      while(current_state.armed &&  ros::Time::now() - time_start1 < ros::Duration(30.0) &&
+            ros::Time::now() - time_start1 > ros::Duration(15.0) &&
+            countPosition == 1){
+        local_pos_pub.publish(pose1);
+
+      }
+
+      //local_vel_pub.publish(vel);
+
+
+
+      ros::spinOnce();
+      rate.sleep();
     }
-
-    // Update the desired pose:
-    pose.pose.position.x = 9*sin(2.0*M_PI*0.3*(ros::Time::now()-time_start).toSec());
-    pose.pose.position.y = 9*cos(2.0*M_PI*0.3*(ros::Time::now()-time_start).toSec());
-
-    //Update the desired velocity:
-    vel.linear.x = 4.0*M_PI*0.1*cos(2.0*M_PI*0.1*(ros::Time::now()-time_start).toSec());
-    vel.linear.y = -4.0*M_PI*0.1*sin(2.0*M_PI*0.1*(ros::Time::now()-time_start).toSec());
-
-    local_pos_pub.publish(pose);
-    local_vel_pub.publish(vel);
-
-
-
-    ros::spinOnce();
-    rate.sleep();
-  }
 
   return 0;
 }
