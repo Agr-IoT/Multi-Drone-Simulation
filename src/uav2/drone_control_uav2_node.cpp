6dcbe73c39d2f9d7cdbf2c64b2d1a63bc3ce0f50
zameel <zameelnm96@gmail.com>
2020-05-22 12:45:23 +0530

    path of uav2 defined

--- src/uav2/drone_control_uav2_node.cpp
+++ src/uav2/drone_control_uav2_node.cpp
@@ -11,7 +11,7 @@ void state_cb(const mavros_msgs::State::ConstPtr& msg){
 }
 
 void testm(){
-  
+
 }
 
 int main(int argc, char **argv)
@@ -36,18 +36,20 @@ int main(int argc, char **argv)
         ros::spinOnce();
         rate.sleep();
     }
-    ROS_INFO("connected...");
+    ROS_INFO("Connected.");
 
     geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
+    pose.header.frame_id = "map";
+    pose.pose.position.x = 5;
+    pose.pose.position.y = 5;
     pose.pose.position.z = 2;
+
 
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
-        local_pos_pub.publish(pose);
-        ros::spinOnce();
-        rate.sleep();
+      local_pos_pub.publish(pose);
+      ros::spinOnce();
+      rate.sleep();
     }
 
     mavros_msgs::SetMode offb_set_mode;
@@ -56,36 +58,176 @@ int main(int argc, char **argv)
     mavros_msgs::CommandBool arm_cmd;
     arm_cmd.request.value = true;
 
-    ros::Time last_request = ros::Time::now();
-    ros::Time time_start = ros::Time::now();
 
+    geometry_msgs::PoseStamped sensor1Position;
+    sensor1Position.pose.position.x = 6;
+    sensor1Position.pose.position.y = 9;
+    sensor1Position.pose.position.z = 2;
+
+    geometry_msgs::PoseStamped sensor2Position;
+    sensor2Position.pose.position.x = 10;
+    sensor2Position.pose.position.y = 7;
+    sensor2Position.pose.position.z = 2;
+
+    geometry_msgs::PoseStamped sensor3Position;
+    sensor3Position.pose.position.x = 12;
+    sensor3Position.pose.position.y = 4;
+    sensor3Position.pose.position.z = 1;
+
+    geometry_msgs::PoseStamped sensor4Position;
+    sensor4Position.pose.position.x = 10;
+    sensor4Position.pose.position.y = 4;
+    sensor4Position.pose.position.z = 2;
+
+    geometry_msgs::PoseStamped sensor5Position;
+    sensor5Position.pose.position.x = 5;
+    sensor5Position.pose.position.y = 2;
+    sensor5Position.pose.position.z = 2;
+
+    geometry_msgs::PoseStamped sensor6Position;
+    sensor6Position.pose.position.x = 0;
+    sensor6Position.pose.position.y = 2;
+    sensor6Position.pose.position.z = 3;
+
+    geometry_msgs::PoseStamped sensor7Position;
+    sensor7Position.pose.position.x = 1;
+    sensor7Position.pose.position.y = 8;
+    sensor7Position.pose.position.z = 2;
+
+
+
+
+
+    ros::Time last_request = ros::Time::now();
+
+    ros::Time time_start = ros::Time::now();
+    ros::Time time_start1 = ros::Time::now();
+    bool isArnmed = false;
+    int  countPosition = 0;
     while(ros::ok()){
+
+        ros::Time loop_time_start = ros::Time::now();
         if( current_state.mode != "OFFBOARD" &&
             (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent){
-                ROS_INFO("Offboard enabled");
-            }
-            last_request = ros::Time::now();
-        } else {
-            if( !current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success){
-                    ROS_INFO("Vehicle armed");
+                if( set_mode_client.call(offb_set_mode) &&
+                      offb_set_mode.response.mode_sent){
+                      ROS_INFO("Offboard enabled");
+
                 }
                 last_request = ros::Time::now();
-            }
+        } else {
+                if( !current_state.armed &&
+              (ros::Time::now() - last_request > ros::Duration(5.0))){
+                    if( arming_client.call(arm_cmd) &&
+                        arm_cmd.response.success){
+                      ROS_INFO("Vehicle armed");
+                      time_start1 = ros::Time::now();
+                    }
+                     last_request = ros::Time::now();
+                }
         }
 
-        pose.pose.position.x = 5*sin(2.0*M_PI*0.1*(ros::Time::now()-time_start).toSec());
-        pose.pose.position.y = 5*cos(2.0*M_PI*0.1*(ros::Time::now()-time_start).toSec());
+        // Update the desired pose:
+        /*pose.pose.position.x = 9*sin(2.0*M_PI*0.3*(ros::Time::now()-time_start).toSec());
+        pose.pose.position.y = 9*cos(2.0*M_PI*0.3*(ros::Time::now()-time_start).toSec());
+        //Update the desired velocity:
+        vel.linear.x = 4.0*M_PI*0.1*cos(2.0*M_PI*0.1*(ros::Time::now()-time_start).toSec());
+        vel.linear.y = -4.0*M_PI*0.1*sin(2.0*M_PI*0.1*(ros::Time::now()-time_start).toSec());*/
+        if(countPosition == 0){
 
-        local_pos_pub.publish(pose);
+          for(int i = 100; ros::ok() && i > 0; --i){
+            local_pos_pub.publish(pose);
+            ros::spinOnce();
+            rate.sleep();
+          }
+        }
+        else if(countPosition == 1){
+
+          for(int i = 100; ros::ok() && i > 0; --i){
+            local_pos_pub.publish(sensor1Position);
+            ros::spinOnce();
+            rate.sleep();
+          }
+
+        }
+
+        else if(countPosition == 2){
+
+          for(int i = 100; ros::ok() && i > 0; --i){
+            local_pos_pub.publish(sensor2Position);
+            ros::spinOnce();
+            rate.sleep();
+          }
+
+        }
+
+        else if(countPosition == 3){
+
+          for(int i = 100; ros::ok() && i > 0; --i){
+            local_pos_pub.publish(sensor3Position);
+            ros::spinOnce();
+            rate.sleep();
+          }
+
+        }
+        else if(countPosition == 4){
+
+          for(int i = 100; ros::ok() && i > 0; --i){
+            local_pos_pub.publish(sensor4Position);
+            ros::spinOnce();
+            rate.sleep();
+          }
+
+        }
+        else if(countPosition == 5){
+
+          for(int i = 100; ros::ok() && i > 0; --i){
+            local_pos_pub.publish(sensor5Position);
+            ros::spinOnce();
+            rate.sleep();
+          }
+
+        }
+        else if(countPosition == 6){
+
+          for(int i = 100; ros::ok() && i > 0; --i){
+            local_pos_pub.publish(sensor6Position);
+            ros::spinOnce();
+            rate.sleep();
+          }
+
+        }
+        else if(countPosition == 7){
+
+          for(int i = 100; ros::ok() && i > 0; --i){
+            local_pos_pub.publish(sensor7Position);
+            ros::spinOnce();
+            rate.sleep();
+          }
+
+        }
+
+
+        if(current_state.armed && ros::Time::now() - time_start1 > ros::Duration(4.0)){
+          std::cout << "position is" + std::to_string(countPosition) << '\n';
+          countPosition += 1  ;
+
+          if(countPosition > 7){
+            countPosition = 0;
+            ROS_INFO("countPosition 0"  );
+            time_start1 = ros::Time::now();
+          }
+        }
+
+        //local_pos_pub.publish(pose);
+
+        //local_vel_pub.publish(vel);
+
+
 
         ros::spinOnce();
         rate.sleep();
-    }
+      }
 
     return 0;
 }
